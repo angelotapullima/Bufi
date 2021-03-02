04b1c590682891ecf4ff6473fd041d6a2b1849b5
WendySol <wmarisol93@gmail.com>
2021-03-02 13:59:14 -0500

    compartir

--- lib/src/page/Tabs/Usuario/Pedidos/RatingPage.dart
+++ lib/src/page/Tabs/Usuario/Pedidos/RatingPage.dart
@@ -2,12 +2,12 @@ import 'dart:io';
 
 import 'package:bufi/src/api/pedidos/Pedido_api.dart';
 import 'package:bufi/src/models/PedidosModel.dart';
-import 'package:bufi/src/models/productoModel.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:bufi/src/utils/responsive.dart';
+import 'package:bufi/src/utils/utils.dart' as utils;
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-
-
+import 'package:image_picker/image_picker.dart';
 
 class RatingProductosPage extends StatefulWidget {
   RatingProductosPage({Key key}) : super(key: key);
@@ -17,14 +17,22 @@ class RatingProductosPage extends StatefulWidget {
 }
 
 class _RatingProductosPageState extends State<RatingProductosPage> {
+  double valueRating = 0.0;
+  //widget para el cargando
+  final _cargando = ValueNotifier<bool>(false);
+
+  //Para acceder a la galeria o tomar una foto
+  File foto;
+  //Usar en cropper
+  bool _inProcess = false;
+
   @override
   Widget build(BuildContext context) {
     final responsive = Responsive.of(context);
     final PedidosModel pedido = ModalRoute.of(context).settings.arguments;
 
-  TextEditingController comentarioController = TextEditingController();
-  File foto;
-  
+    TextEditingController comentarioController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -35,153 +43,255 @@ class _RatingProductosPageState extends State<RatingProductosPage> {
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.only(
-              top: responsive.ip(4),
-              left: responsive.ip(2),
-              right: responsive.ip(2)),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Califica el producto",
-                  style: TextStyle(fontSize: responsive.ip(2)),
-                ),
-
-                RatingBar.builder(
-                  initialRating: 3,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: Colors.amber,
-                  ),
-                  onRatingUpdate: (rating) {
-                    print(rating);
-                  },
-                ),
-
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                //   children: [
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //   ],
-                // ),
-                SizedBox(height: responsive.hp(2)),
-                Text("Puedes dejarnos un comentario (100 caracteres)",
-                    style: TextStyle(fontSize: responsive.ip(2))),
-                SizedBox(height: responsive.hp(1)),
-                Container(
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: TextField(
-                    controller: comentarioController,
-                    keyboardType: TextInputType.text,
-                    maxLines: 3,
-                  ),
-                ),
-                SizedBox(height: responsive.hp(4)),
-                Row(
-                  children: [
-                    IconButton(
-                        icon: Icon(
-                          Icons.camera_alt_rounded,
+      body: ValueListenableBuilder(
+        valueListenable: _cargando,
+        builder: (BuildContext context, bool value, Widget child) {
+          return Stack(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: responsive.ip(4),
+                      left: responsive.ip(2),
+                      right: responsive.ip(2)),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Califica el producto",
+                          style: TextStyle(fontSize: responsive.ip(2)),
                         ),
-                        onPressed: () {}),
-                    RaisedButton(
-                      onPressed: () {},
-                      child: Text("Subir Fotos"),
-                    )
-                  ],
-                ),
-                SizedBox(height: responsive.hp(2)),
-                // Text("Calificación detallada",
-                //     style: TextStyle(fontSize: responsive.ip(2.2))),
-                // Divider(),
-                // SizedBox(height: responsive.hp(2)),
-                // Text("¿Qué tan precisa fue la descripción del producto?",
-                //     style: TextStyle(fontSize: responsive.ip(2))),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                //   children: [
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //   ],
-                // ),
-                // Text("¿Qué tan rápido envió el vendedor el artículo?",
-                //     style: TextStyle(fontSize: responsive.ip(2))),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                //   children: [
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //   ],
-                // ),
-                // Text("¿Está satisfecho con la comunicación con el vendedor?",
-                //     style: TextStyle(fontSize: responsive.ip(2))),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                //   children: [
-                //     IconButton(
-                //         icon: Icon(
-                //           Icons.star,
-                //           color: Colors.yellow,
-                //         ),
-                //         onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //     IconButton(icon: Icon(Icons.star), onPressed: () {}),
-                //   ],
-                // ),
-                SizedBox(height: responsive.hp(2)),
-                Center(
-                  child: SizedBox(
-                    width: responsive.wp(80),
-                    child: RaisedButton(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      color: Colors.red,
-                      onPressed: () {
-                        final pedidoApi= PedidoApi();
-                        final producModel= ProductoModel();
-                        final pedidoModel= PedidosModel();
-                       
-                        final code= pedidoApi.valoracion(foto, producModel, pedidoModel, comentarioController);
-                        if (code==1) {
-                          
-                          
-                        }
-                        
-                      },
-                      child: Text("Calificar",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: responsive.ip(2.2))),
+                        RatingBar.builder(
+                          initialRating: 0,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (rating) {
+                            valueRating = rating;
+
+                            print(rating);
+                          },
+                        ),
+                        SizedBox(height: responsive.hp(2)),
+                        Text("Puedes dejarnos un comentario (100 caracteres)",
+                            style: TextStyle(fontSize: responsive.ip(2))),
+                        SizedBox(height: responsive.hp(1)),
+                        Container(
+                          decoration: BoxDecoration(border: Border.all()),
+                          child: TextField(
+                            controller: comentarioController,
+                            keyboardType: TextInputType.text,
+                            maxLines: 3,
+                          ),
+                        ),
+                        SizedBox(height: responsive.hp(4)),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.camera_alt_rounded,
+                            ),
+                            SizedBox(width: responsive.wp(2)),
+                            RaisedButton(
+                              onPressed: () {
+                                _subirFoto(context);
+                              },
+                              child: Text("Subir Fotos"),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: responsive.hp(2)),
+                        _mostrarFoto(responsive),
+                        SizedBox(height: responsive.hp(2)),
+                        Center(
+                          child: SizedBox(
+                            width: responsive.wp(80),
+                            child: RaisedButton(
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0)),
+                              color: Colors.red,
+                              child: Text("Calificar",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: responsive.ip(2.2))),
+                              onPressed: () async {
+                                await _submit(
+                                    pedido, comentarioController, context);
+                              },
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
-        ),
+                ),
+              ),
+              (value)? Container(
+                      height: double.infinity,
+                      color: Colors.white54,
+                      child: Center(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: responsive.wp(10)),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: responsive.wp(10)),
+                          width: double.infinity,
+                          height: responsive.hp(13),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: responsive.wp(10),
+                                vertical: responsive.wp(6)),
+                            height: responsive.ip(4),
+                            width: responsive.ip(4),
+                            child: Image(
+                                image: AssetImage('assets/cargando.gif'),
+                                fit: BoxFit.contain),
+                          ),
+                        ),
+                      ),
+                    )
+                  : Container()
+            ],
+          );
+        },
       ),
     );
+  }
+
+  Future _subirFoto(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Seleccione"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text("Galeria"),
+                    onTap: () {
+                      Navigator.pop(context);
+                      getImage(ImageSource.gallery);
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child: Text("Camara"),
+                    onTap: () {
+                      Navigator.pop(context);
+                      getImage(ImageSource.camera);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future _submit(PedidosModel pedido,
+      TextEditingController comentarioController, BuildContext context) async {
+    final pedidoApi = PedidoApi();
+
+    if (foto != null) {
+      if (comentarioController.text != null) {
+        if (valueRating != null) {
+           _cargando.value = true;
+          final int code = await pedidoApi.valoracion(
+              foto,
+              pedido.detallePedido[0].idProducto,
+              pedido.idPedido,
+              comentarioController.text,
+              valueRating.toString());
+          if (code == 1) {
+            print("Producto Calificado");
+            utils.showToast(context, 'Producto Calificado');
+             Navigator.pop(context);
+          } else {
+            utils.showToast(context, 'Faltan ingresar datos');
+          }
+
+           _cargando.value = false;
+        } else {
+          utils.showToast(context, 'Debe ingresar su comentario');
+        }
+      } else {
+        utils.showToast(context, 'Debe ingresar su comentario');
+      }
+    } else {
+      utils.showToast(context, 'Debe seleccionar una foto');
+    }
+  }
+
+  getImage(ImageSource source) async {
+    this.setState(() {
+      _inProcess = true;
+    });
+    File image = await ImagePicker.pickImage(source: source);
+    if (image != null) {
+      File cropped = await ImageCropper.cropImage(
+        sourcePath: image.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.ratio3x2,
+              ]
+            : [
+                CropAspectRatioPreset.ratio3x2,
+              ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Recortar',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: true),
+        iosUiSettings: IOSUiSettings(
+          title: 'Imagen',
+        ),
+
+        // maxWidth: 20,
+        // maxHeight: 30
+      );
+
+      this.setState(() {
+        foto = cropped;
+        _inProcess = false;
+      });
+    } else {
+      this.setState(() {
+        _inProcess = false;
+      });
+    }
+  }
+
+  Widget _mostrarFoto(Responsive responsive) {
+    if (foto != null) {
+      return Container(
+        height: responsive.hp(10),
+        width: responsive.wp(20),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image(
+            image: AssetImage(foto.path),
+            height: responsive.hp(38),
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 }
